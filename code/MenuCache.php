b6151bdad75e13a2c0fbd3e709ee3d51a8aaacca
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 13:42:45 +1300

    PATCH: module clean-up

--- code/MenuCache.php
+++ code/MenuCache.php
@@ -1,155 +1,159 @@
 <?php
 
-class MenuCache extends DataExtension {
-	/**
-	* fields are typicall header, menu, footer
-	*/
+class MenuCache extends DataExtension
+{
+    /**
+    * fields are typicall header, menu, footer
+    */
 
-	private static $db = array(
-		"CachedSection0" => "HTMLText",
-		"CachedSection1" => "HTMLText",
-		"CachedSection2" => "HTMLText",
-		"CachedSection3" => "HTMLText",
-		"CachedSection4" => "HTMLText"
-	);
+    private static $db = array(
+        "CachedSection0" => "HTMLText",
+        "CachedSection1" => "HTMLText",
+        "CachedSection2" => "HTMLText",
+        "CachedSection3" => "HTMLText",
+        "CachedSection4" => "HTMLText"
+    );
 
-	private static $fields = array(
-		0 => "Header",
-		1 => "Menu",
-		2 => "Footer",
-		3 => "LayoutSection",
-		4 => "other",
-	);
+    private static $fields = array(
+        0 => "Header",
+        1 => "Menu",
+        2 => "Footer",
+        3 => "LayoutSection",
+        4 => "other",
+    );
 
-	/* sets the cache number used for getting the "$Layout" of the individual page */
-	private static $layout_field = 3;
+    /* sets the cache number used for getting the "$Layout" of the individual page */
+    private static $layout_field = 3;
 
-	private static $tables_to_clear = array("SiteTree", "SiteTree_Live", "SiteTree_versions");
+    private static $tables_to_clear = array("SiteTree", "SiteTree_Live", "SiteTree_versions");
 
-	public static function field_maker($fieldNumber) {
-		return "CachedSection".$fieldNumber;
-	}
+    public static function field_maker($fieldNumber)
+    {
+        return "CachedSection".$fieldNumber;
+    }
 
-	public static function fields_exists($number) {
-		return (isset(self::$fields[$number]));
-	}
+    public static function fields_exists($number)
+    {
+        return (isset(self::$fields[$number]));
+    }
 
-	function updateCMSFields(FieldList $fields) {
-		$fields->addFieldToTab("Root.Caching", new CheckboxField("DoNotCacheMenu","Do Not Cache Menu"));
-		$fields->addFieldToTab("Root.Caching", new LiteralField("ClearCache","<a href=\"".$this->owner->Link("clearallfieldcaches")."\">clear cache (do this at the end of all edit sessions)</a>"));
-		return $fields;
-	}
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldToTab("Root.Caching", new CheckboxField("DoNotCacheMenu", "Do Not Cache Menu"));
+        $fields->addFieldToTab("Root.Caching", new LiteralField("ClearCache", "<a href=\"".$this->owner->Link("clearallfieldcaches")."\">clear cache (do this at the end of all edit sessions)</a>"));
+        return $fields;
+    }
 
-	//-------------------- menu cache ------------------ ------------------ ------------------ ------------------ ------------------ ------------------
+    //-------------------- menu cache ------------------ ------------------ ------------------ ------------------ ------------------ ------------------
 
-	function clearfieldcache ($showoutput = false) {
-		$fieldsToClear = array();
-		$fieldsForEach = Config::inst()->get("MenuCache", "fields");
-		foreach($fieldsForEach as $key => $field) {
-			$fieldName = self::field_maker($key);
-			$fieldsToClear[] = "\"".$fieldName."\" = ''";
-		}
-		if(count($fieldsToClear)) {
-			$tablesForEach = Config::inst()->get("MenuCache", "tables_to_clear");
-			foreach($tablesForEach as $table) {
-				$msg = '';
-				$sql = "UPDATE \"".$table."\" SET ".implode(", ", $fieldsToClear);
-				if( Controller::curr()->getRequest()->param("ID") == "days" && $days = intval(Controller::curr()->getRequest()->param("OtherID"))) {
-					$sql .= ' WHERE \"LastEdited\" > ( NOW() - INTERVAL '.$days.' DAY )';
-					$msg .= ', created before the last '.$days.' days';
-				}
-				elseif(Controller::curr()->getRequest()->param("ID") == "thispage") {
-					$sql .= " WHERE  \"".$table."\".\"ID\" = ".$this->owner->ID;
-					$msg .= ', for page with ID = '.$this->owner->ID;
-				}
-				if($showoutput) {
-					DB::alteration_message("Deleting cached data from $table, ".$msg);
-					debug::show($sql);
-				}
-				DB::query($sql);
-			}
-		}
-		return array();
-	}
+    public function clearfieldcache($showoutput = false)
+    {
+        $fieldsToClear = array();
+        $fieldsForEach = Config::inst()->get("MenuCache", "fields");
+        foreach ($fieldsForEach as $key => $field) {
+            $fieldName = self::field_maker($key);
+            $fieldsToClear[] = "\"".$fieldName."\" = ''";
+        }
+        if (count($fieldsToClear)) {
+            $tablesForEach = Config::inst()->get("MenuCache", "tables_to_clear");
+            foreach ($tablesForEach as $table) {
+                $msg = '';
+                $sql = "UPDATE \"".$table."\" SET ".implode(", ", $fieldsToClear);
+                if (Controller::curr()->getRequest()->param("ID") == "days" && $days = intval(Controller::curr()->getRequest()->param("OtherID"))) {
+                    $sql .= ' WHERE \"LastEdited\" > ( NOW() - INTERVAL '.$days.' DAY )';
+                    $msg .= ', created before the last '.$days.' days';
+                } elseif (Controller::curr()->getRequest()->param("ID") == "thispage") {
+                    $sql .= " WHERE  \"".$table."\".\"ID\" = ".$this->owner->ID;
+                    $msg .= ', for page with ID = '.$this->owner->ID;
+                }
+                if ($showoutput) {
+                    DB::alteration_message("Deleting cached data from $table, ".$msg);
+                    debug::show($sql);
+                }
+                DB::query($sql);
+            }
+        }
+        return array();
+    }
 
-	//add this function to your page class if needed
-	function onBeforeWrite() {
-		//$this->clearfieldcache(); // technically this should be done, but it puts a lot of strain on saving so instead we encourage people to use ?flush=1
-		parent::onBeforeWrite();
-	}
-
-
+    //add this function to your page class if needed
+    public function onBeforeWrite()
+    {
+        //$this->clearfieldcache(); // technically this should be done, but it puts a lot of strain on saving so instead we encourage people to use ?flush=1
+        parent::onBeforeWrite();
+    }
 }
 
-class MenuCache_Controller extends Extension {
+class MenuCache_Controller extends Extension
+{
+    private static $allowed_actions = array("showcachedfield","clearfieldcache","showuncachedfield", "clearallfieldcaches");
 
-	private static $allowed_actions = array("showcachedfield","clearfieldcache","showuncachedfield", "clearallfieldcaches");
+    protected function getHtml($fieldNumber)
+    {
+        $layoutField = Config::inst()->get("MenuCache", "layout_field");
+        if ($layoutField == $fieldNumber) {
+            $className = $this->owner->ClassName;
+            if ("Page" == $className) {
+                $className = "PageCached";
+            }
+            return $this->owner->renderWith(array($className, "PageCached"));
+        } else {
+            return $this->owner->renderWith('UsedToCreateCache'.$fieldNumber);
+        }
+    }
 
-	protected function getHtml($fieldNumber) {
-		$layoutField = Config::inst()->get("MenuCache", "layout_field");
-		if($layoutField == $fieldNumber) {
-			$className = $this->owner->ClassName;
-			if("Page" == $className) {
-				$className = "PageCached";
-			}
-			return $this->owner->renderWith(array($className, "PageCached"));
-		}
-		else {
-			return $this->owner->renderWith('UsedToCreateCache'.$fieldNumber);
-		}
-	}
-
-	function CachedField($fieldNumber) {
-		$fieldName = MenuCache::field_maker($fieldNumber);
-		if(isset($_REQUEST["flush"])) {
-			$this->owner->clearfieldcache();
-		}
-		if(!(MenuCache::fields_exists($fieldNumber))) {
-			user_error("$fieldName is not a field that can be cached", E_USER_ERROR);
-		}
-		else {
-			if(!$this->owner->$fieldName || $this->owner->DoNotCacheMenu) {
-				$fieldID = $fieldNumber;
-				$content = $this->getHtml($fieldNumber);
-				$sql = "Update \"SiteTree_Live\" Set \"".$fieldName."\" = '".$this->compressAndPrepareHTML($content)."' WHERE \"ID\" = ".$this->owner->ID." LIMIT 1";
-				DB::query($sql);
-				return $content;
-			}
-			else {
-				return $this->owner->$fieldName;
-			}
-		}
-	}
-
-
-	private function compressAndPrepareHTML($html) {
-		$pat[0] = "/^\s+/";
-		$pat[1] = "/\s{2,}/";
-		$pat[2] = "/\s+\$/";
-		$rep[0] = "";
-		$rep[1] = " ";
-		$rep[2] = "";
-		$html = preg_replace($pat, $rep, $html);
-		$html = trim($html);
-		return addslashes($html);
-	}
+    public function CachedField($fieldNumber)
+    {
+        $fieldName = MenuCache::field_maker($fieldNumber);
+        if (isset($_REQUEST["flush"])) {
+            $this->owner->clearfieldcache();
+        }
+        if (!(MenuCache::fields_exists($fieldNumber))) {
+            user_error("$fieldName is not a field that can be cached", E_USER_ERROR);
+        } else {
+            if (!$this->owner->$fieldName || $this->owner->DoNotCacheMenu) {
+                $fieldID = $fieldNumber;
+                $content = $this->getHtml($fieldNumber);
+                $sql = "Update \"SiteTree_Live\" Set \"".$fieldName."\" = '".$this->compressAndPrepareHTML($content)."' WHERE \"ID\" = ".$this->owner->ID." LIMIT 1";
+                DB::query($sql);
+                return $content;
+            } else {
+                return $this->owner->$fieldName;
+            }
+        }
+    }
 
 
-
-	function showcachedfield($httpRequest = null) {
-		$fieldNumber = $httpRequest->param("ID");
-		return $this->getHtml($fieldNumber);
-	}
-
-	function showuncachedfield($httpRequest = null) {
-		$this->owner->clearfieldcache();
-		return $this->showcachedfield($httpRequest);
-	}
-
-	function clearallfieldcaches($httpRequest = null) {
-		$this->owner->clearfieldcache(true);
-		return 'fields have been cleared, <a href="/?flush=all">click to continue...</a>';
-	}
+    private function compressAndPrepareHTML($html)
+    {
+        $pat[0] = "/^\s+/";
+        $pat[1] = "/\s{2,}/";
+        $pat[2] = "/\s+\$/";
+        $rep[0] = "";
+        $rep[1] = " ";
+        $rep[2] = "";
+        $html = preg_replace($pat, $rep, $html);
+        $html = trim($html);
+        return addslashes($html);
+    }
 
 
+
+    public function showcachedfield($httpRequest = null)
+    {
+        $fieldNumber = $httpRequest->param("ID");
+        return $this->getHtml($fieldNumber);
+    }
+
+    public function showuncachedfield($httpRequest = null)
+    {
+        $this->owner->clearfieldcache();
+        return $this->showcachedfield($httpRequest);
+    }
+
+    public function clearallfieldcaches($httpRequest = null)
+    {
+        $this->owner->clearfieldcache(true);
+        return 'fields have been cleared, <a href="/?flush=all">click to continue...</a>';
+    }
 }
--- code/MenuCacheStatic.php
+++ code/MenuCacheStatic.php
@@ -1,77 +1,89 @@
 <?php
 
-class MenuCacheStatic extends DataExtension {
+class MenuCacheStatic extends DataExtension
+{
+    private static $class_names_to_cache = array();
+    public static function add_class_name_to_cache($className)
+    {
+        self::$class_names_to_cache[$className] = $className;
+    }
 
-	private static $class_names_to_cache = array();
-		static function add_class_name_to_cache($className) {self::$class_names_to_cache[$className] = $className;}
+    private static $class_names_NOT_to_cache = array();
+    public static function remove_class_name_from_cache($className)
+    {
+        self::$class_names_NOT_to_cache[$className] = $className;
+    }
+    public static function get_class_names_NOT_to_cache($className)
+    {
+        self::$class_names_NOT_to_cache["UserDefinedForm"] = "UserDefinedForm";
+        return self::$class_names_NOT_to_cache;
+    }
 
-	private static $class_names_NOT_to_cache = array();
-		static function remove_class_name_from_cache($className) {self::$class_names_NOT_to_cache[$className] = $className;}
-		static function get_class_names_NOT_to_cache($className) {
-			self::$class_names_NOT_to_cache["UserDefinedForm"] = "UserDefinedForm";
-			return self::$class_names_NOT_to_cache;
-		}
+    private static $custom_urls_to_add = array();
+    public static function add_custom_url_to_cache($url)
+    {
+        self::$custom_urls_to_add[$url] = $url;
+    }
 
-	private static $custom_urls_to_add = array();
-		static function add_custom_url_to_cache($url) {self::$custom_urls_to_add[$url] = $url;}
+    public function updateCMSFields(FieldList $fields)
+    {
+        $js = "window.open(this.href, 'publisher', 'toolbar=0,scrollbars=1,location=0,statusbar=1,menubar=0,resizable=0,width=600,height=600'); return false;";
+        $fields->addFieldToTab("Root.Caching", new LiteralField("PublishAllPages", '<p>This website runs a static cache to make it faster.  You may be required to manually publish the pages to the cache. You can <a href="/dev/buildcache/?flush=1" onclick="'.$js.'">publish all pages to cache</a> now...</p>'));
+        return $fields;
+    }
 
-	function updateCMSFields(FieldList $fields) {
-		$js = "window.open(this.href, 'publisher', 'toolbar=0,scrollbars=1,location=0,statusbar=1,menubar=0,resizable=0,width=600,height=600'); return false;";
-		$fields->addFieldToTab("Root.Caching", new LiteralField("PublishAllPages",'<p>This website runs a static cache to make it faster.  You may be required to manually publish the pages to the cache. You can <a href="/dev/buildcache/?flush=1" onclick="'.$js.'">publish all pages to cache</a> now...</p>'));
-		return $fields;
-	}
-
-	//------------------ static publisher ------------------ ------------------ ------------------ ------------------
-	/**
-	 * Return a list of all the pages to cache
-	 */
-	function allPagesToCache() {
-		// Get each page type to define its sub-urls
-		$urls = array();
-		// memory intensive depending on number of pages
-		foreach(self::$class_names_to_cache as $className) {
-			$pages = $className::get()
-				->exclude(array("ClassName" => self::$class_names_NOT_to_cache));
-			if($pages->count()) {
-				foreach($pages as $page) {
-					$urls = array_merge($urls, (array)$page->subPagesToCache());
-				}
-			}
-		}
-		if(is_array(self::$custom_urls_to_add)) {
-			if(count(self::$custom_urls_to_add)) {
-				foreach(self::$custom_urls_to_add as $url) {
-					$urls[] = $url;
-				}
-			}
-		}
-		return $urls;
-	}
+    //------------------ static publisher ------------------ ------------------ ------------------ ------------------
+    /**
+     * Return a list of all the pages to cache
+     */
+    public function allPagesToCache()
+    {
+        // Get each page type to define its sub-urls
+        $urls = array();
+        // memory intensive depending on number of pages
+        foreach (self::$class_names_to_cache as $className) {
+            $pages = $className::get()
+                ->exclude(array("ClassName" => self::$class_names_NOT_to_cache));
+            if ($pages->count()) {
+                foreach ($pages as $page) {
+                    $urls = array_merge($urls, (array)$page->subPagesToCache());
+                }
+            }
+        }
+        if (is_array(self::$custom_urls_to_add)) {
+            if (count(self::$custom_urls_to_add)) {
+                foreach (self::$custom_urls_to_add as $url) {
+                    $urls[] = $url;
+                }
+            }
+        }
+        return $urls;
+    }
 
  /**
-	 * Get a list of URLs to cache related to this page
-	 */
-	function subPagesToCache() {
-		$urls = array();
+     * Get a list of URLs to cache related to this page
+     */
+    public function subPagesToCache()
+    {
+        $urls = array();
 
-		// add current page
-		$urls[] = $this->owner->Link();
+        // add current page
+        $urls[] = $this->owner->Link();
 
-		// cache the RSS feed if comments are enabled
-		if ($this->owner->ProvideComments) {
-			$urls[] = Director::absoluteBaseURL() . "pagecomment/rss/" . $this->owner->ID;
-		}
+        // cache the RSS feed if comments are enabled
+        if ($this->owner->ProvideComments) {
+            $urls[] = Director::absoluteBaseURL() . "pagecomment/rss/" . $this->owner->ID;
+        }
 
-		return $urls;
-	}
+        return $urls;
+    }
 
-	function pagesAffectedByChanges() {
-		$urls = $this->subPagesToCache();
-		if($parent = SiteTree::get()->byID($this->owner->ParentID)) {
-			$urls = array_merge((array)$urls, (array)$parent->subPagesToCache());
-		}
-		return $urls;
-	}
-
+    public function pagesAffectedByChanges()
+    {
+        $urls = $this->subPagesToCache();
+        if ($parent = SiteTree::get()->byID($this->owner->ParentID)) {
+            $urls = array_merge((array)$urls, (array)$parent->subPagesToCache());
+        }
+        return $urls;
+    }
 }
-
